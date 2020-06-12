04473b07567421676ab7e9a5cdb272415ee41e24
michhottinger <michhottinger@users.noreply.github.com>
2020-06-12 15:37:55 -0700

    added bst to names

--- names/.ipynb_checkpoints/names-checkpoint.py
+++ names/.ipynb_checkpoints/names-checkpoint.py
@@ -22,17 +22,81 @@ duplicates = []  # Return the list of duplicates in this data structure
 # runtime: 7.768748998641968 seconds
 
 #my solution:
-# while name_1 in names_1 == name_2 in names_2:
-#     duplicates.append(name_1)
-# duplicates = [i for i in names_1 if i in names_2]
+#Set('abc').intersection('cbs') from: https://docs.python.org/2/library/sets.html
+#this is brilliant
       
 #the right solution O(n)  = O(n + n + n)  
     
-duplicates = list(set(names_1).intersection(set(names_2)))#three linear operations that are O(n)
+#duplicates = list(set(names_1).intersection(set(names_2)))#three linear operations that are O(n)
 #intersection is a math term that means finds dupes and return them
 
 #runtime: 0.007816076278686523 seconds
-    
+
+
+
+class BSTNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    # Insert the given value into the tree
+    def insert(self, value):
+        #check incoming value
+        if value < self.value:
+            if not self.left:
+                self.left = BSTNode(value)
+            else:
+                self.left.insert(value)#resets the root to the left node then reruns
+        else:
+            if not self.right:
+                self.right = BSTNode(value)
+            else:
+                self.right.insert(value)
+
+    # Return True if the tree contains the value
+    # False if it does not
+    def contains(self, target):
+        if target == self.value:
+            return True
+        if target < self.value:
+            #go left if left is a BSTNode
+            if not self.left:
+                return False
+            return self.left.contains(target)
+        else:
+            #got right if the right is a BSTNode
+            if not self.right:
+                return False
+            return self.right.contains(target)
+
+    # Return the maximum value found in the tree
+    def get_max(self):
+        if not self.right:
+            return self.value
+        return self.right.get_max()
+
+    # Call the function `fn` on the value of each node
+    def for_each(self, fn):
+        fn(self.value)
+        #pass func to left child
+        if self.left:
+            self.left.for_each(fn)
+        #pass func to the right child
+        if self.right:
+            self.right.for_each(fn)
+            
+bst = BSTNode("")
+
+for name in names_1:
+    bst.insert(name)
+
+for name in names_2:
+    if bst.contains(name):
+        duplicates.append(name)
+
+
+
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
