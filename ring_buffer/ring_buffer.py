42b5ec05aeaee31133fac20fcd49bb143f84b67d
michhottinger <michhottinger@users.noreply.github.com>
2020-06-12 09:30:05 -0700

    changed some notes

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,7 @@ class RingBuffer:
         self.storage = [None]*capacity #learn this format here: https://stackoverflow.com/questions/10155510/how-to-grow-a-list-to-fit-a-given-capacity-in-python
 #it works great!
     def append(self, item):
-        self.storage[self.current] = item#index the storage at the current and make equal to item
+        self.storage[self.current] = item#storage at the current and make equal to item
         if self.current == self.capacity-1:#current node is at last spot in capacity
             self.current = 0#then current will go to the first in location indexed at 0
         else:
